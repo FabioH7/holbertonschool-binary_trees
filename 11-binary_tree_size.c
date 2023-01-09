e7197c82fc8b99406c0ac0a236400669efdd5abe
Katrolli <ennio_cano@live.com>
2023-01-09 08:00:25 -0800

    size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,13 +2,10 @@
 #include <stddef.h>
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t count_l, count_r;
 	if (tree == NULL)
 		return 0;
-	else
-	{
-		size_t count_l = binary_tree_size(tree->left);
-		size_t count_r = binary_tree_size(tree->right);
-		return (count_l + count_r + 1);
-	}
-	return 0;
+	count_l = binary_tree_size(tree->left);
+	count_r = binary_tree_size(tree->right);
+	return (count_l + count_r + 1);
 }
