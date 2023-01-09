69790c8f8a1467c0c91925d1358fd3ee63a5e46d
Katrolli <ennio_cano@live.com>
2023-01-09 07:58:17 -0800

    size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,8 +3,12 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-        return 0;
-    size_t count_l = binary_tree_size(tree->left);
-    size_t count_r = binary_tree_size(tree->right);
-    return (count_l + count_r + 1);
+		return 0;
+	else
+	{
+		size_t count_l = binary_tree_size(tree->left);
+		size_t count_r = binary_tree_size(tree->right);
+		return (count_l + count_r + 1);
+	}
+	return 0;
 }
