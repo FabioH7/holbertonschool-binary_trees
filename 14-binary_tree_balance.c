df5e12cd495821f006071d7c6d0adf6a0845e8a6
Katrolli <ennio_cano@live.com>
2023-01-09 08:51:20 -0800

    skrrrrah da balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,25 +1,16 @@
 #include "binary_trees.h"
-
-int binary_tree_height_l(const binary_tree_t *tree)
+int binary_tree_height(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return (0);
     else
     {
-        int left_height = binary_tree_height_l(tree->left);
-        return (left_height + 1);
-    }
-    return 0;
-}
-int binary_tree_height_r(const binary_tree_t *tree)
-{
-    if (tree == NULL)
-        return (0);
-    else
-    {
-
-        int right_height = binary_tree_height_r(tree->right);
-        return (right_height + 1);
+        int left_height = binary_tree_height(tree->left);
+        int right_height = binary_tree_height(tree->right);
+        if (left_height >= right_height)
+            return (left_height + 1);
+        else
+            return (right_height + 1);
     }
     return 0;
 }
@@ -27,5 +18,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return 0;
-    return (binary_tree_height_l(tree) - binary_tree_height_r(tree));
+    return (binary_tree_height(tree->left));
 }
