205b03f6cf24195d058abc710c2ceb79ba97faa3
Katrolli <ennio_cano@live.com>
2023-01-10 08:01:57 -0800

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
-int binary_tree_height(const binary_tree_t *tree)
+int binary_tree_height_l(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return (0);
     else
     {
-        int left_height = binary_tree_height(tree->left);
-        int right_height = binary_tree_height(tree->right);
+        int left_height = binary_tree_height_l(tree->left);
+        int right_height = binary_tree_height_l(tree->right);
         if (left_height >= right_height)
             return (left_height + 1);
         else
@@ -22,8 +22,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
     if (tree == NULL)
         return (0);
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height_l(tree->left);
+    right_height = binary_tree_height_l(tree->right);
     if (left_height == right_height)
     {
         return (1);
