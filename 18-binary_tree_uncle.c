7e2c96ad7711dc0b8957afb5a5a1a43f179538ba
Katrolli <ennio_cano@live.com>
2023-01-10 08:23:48 -0800

    uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    if (node == NULL)
+        return NULL;
+    if (node->parent == NULL || node->parent->left == NULL || node->parent->right == NULL)
+        return NULL;
+    if (node->n == node->parent->left->n)
+        return (node->parent->right);
+    return (node->parent->left);
+}
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL)
+        return NULL;
+    if (node->parent == NULL)
+        return NULL;
+    return binary_tree_sibling(node->parent);
+}
