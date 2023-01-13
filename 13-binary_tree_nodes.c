98a0f58e67d3926b0d629ea1ee745b6d28f5b6a0
FabioH7 <fhysollari4@gmail.com>
2023-01-13 01:15:48 -0800

    adding betty for 13 and 14

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - finds the total number of nodes in a tree
+ * @tree: where to count nodes
+ * Return: 0 if tree is null or node count
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count_l, count_r;
-    if (tree == NULL)
-        return 0;
-    count_l = binary_tree_nodes(tree->left);
-    count_r = binary_tree_nodes(tree->right);
-    if (tree->left != NULL || tree->right != NULL)
-        return (count_l + count_r + 1);
-    return (count_l + count_r);
+	size_t count_l, count_r;
+
+	if (tree == NULL)
+		return (0);
+	count_l = binary_tree_nodes(tree->left);
+	count_r = binary_tree_nodes(tree->right);
+	if (tree->left != NULL || tree->right != NULL)
+		return (count_l + count_r + 1);
+	return (count_l + count_r);
 }
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,32 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_height_x - finds tree height
+ * @tree: where to find height
+ * Return: tree height
+ */
 int binary_tree_height_x(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    else
-    {
-        int left_height = binary_tree_height_x(tree->left);
-        int right_height = binary_tree_height_x(tree->right);
-        if (left_height >= right_height)
-            return (left_height + 1);
-        else
-            return (right_height + 1);
-    }
-    return 0;
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height_x(tree->left);
+	right_height = binary_tree_height_x(tree->right);
+	if (left_height >= right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
+
+/**
+ * binary_tree_balance - finds difference between left height
+ * and right height of the tree
+ * @tree: where to find balance
+ * Return: 0 if in balance, positive if left>right, negative int if left<right
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
-    return (binary_tree_height_x(tree->left) - binary_tree_height_x(tree->right));
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_height_x(tree->left) - binary_tree_height_x(tree->right));
 }
