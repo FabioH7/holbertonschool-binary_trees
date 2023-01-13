8c34fbf8d215e4e4a65a35be7f31368e2c2660ae
FabioH7 <fhysollari4@gmail.com>
2023-01-13 01:14:14 -0800

    fixing betty for 7 and 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - prints a binary tree using the inorder method
+ * @tree: tree to print
+ * @func: prints node
+ */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - prints a treee using the postorder method
+ * @tree: tree to print
+ * @func: prints the node
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
