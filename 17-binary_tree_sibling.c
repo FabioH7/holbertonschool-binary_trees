725695331b3f3e01ed48f0c086811ed52ba07e17
FabioH7 <fhysollari4@gmail.com>
2023-01-13 01:16:43 -0800

    adding betty for 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - finds if a node has a sibling
+ * @node: node to search if it has a sibling
+ * Return: sibling node or NULL if not found
+ */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL)
-        return NULL;
-    if (node->parent == NULL || node->parent->left == NULL || node->parent->right == NULL)
-        return NULL;
-    if (node->n == node->parent->left->n)
-        return (node->parent->right);
-    return (node->parent->left);
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL || node->parent->left == NULL
+	|| node->parent->right == NULL)
+		return (NULL);
+	if (node->n == node->parent->left->n)
+		return (node->parent->right);
+	return (node->parent->left);
 }
