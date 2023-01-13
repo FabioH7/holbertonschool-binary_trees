e179230299f1b32177d86913cdbd026ee242651e
FabioH7 <fhysollari4@gmail.com>
2023-01-13 01:13:40 -0800

    fixing betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_root - checks if node is the root of the tree
+ * @node: node to check
+ * Return: 1 if node is the root else returns 0
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder - prints the binary tree in the preorder method
+ * @tree: tree to print
+ * @func: prints node value
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
