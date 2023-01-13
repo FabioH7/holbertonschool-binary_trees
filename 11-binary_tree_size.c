e482547a2822094961e64bd8a76cadd47df46c69
FabioH7 <fhysollari4@gmail.com>
2023-01-13 01:15:18 -0800

    adding betty for 11 and 12

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
 #include <stddef.h>
+
+/**
+ * binary_tree_size - finds the size of a binary tree
+ * @tree: tree to find size
+ * Return: tree size or 0 if tree is null
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t count_l, count_r;
+
 	if (tree == NULL)
-		return 0;
+		return (0);
 	count_l = binary_tree_size(tree->left);
 	count_r = binary_tree_size(tree->right);
 	return (count_l + count_r + 1);
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - finds the number of leaves in a tree
+ * @tree: where to count the leaves
+ * Return: 0 if none or the leaves total number
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t count_l, count_r;
-    if (tree == NULL)
-        return 0;
-    count_l = binary_tree_leaves(tree->left);
-    count_r = binary_tree_leaves(tree->right);
-    if (tree->left == NULL && tree->right == NULL)
-        return (count_l + count_r + 1);
-    return (count_l + count_r);
+	size_t count_l, count_r;
+
+	if (tree == NULL)
+		return (0);
+	count_l = binary_tree_leaves(tree->left);
+	count_r = binary_tree_leaves(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (count_l + count_r + 1);
+	return (count_l + count_r);
 }
