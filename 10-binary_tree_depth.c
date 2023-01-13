309d42dd1ce1db3d4fefd2bcf292ba28bb86f8bf
FabioH7 <fhysollari4@gmail.com>
2023-01-13 01:14:39 -0800

    fixing betty for 9 and 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - finds the depth of a node in a tree
+ * @tree: node of which to find depth
+ * Return: 0 if null or depth
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	int depth;
+
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		int depth = binary_tree_depth(tree->parent);
-
-		if (tree->parent != NULL)
-			return(depth + 1);
-	}
+	depth = binary_tree_depth(tree->parent);
+	if (tree->parent != NULL)
+		return (depth + 1);
 	return (0);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,18 +2,20 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - finds the length of the longest branch of a tree
+ * @tree: parent node of the tree
+ * Return: 0 if tree is null or the length of tree
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL || tree->left == NULL)
 		return (0);
-	else
-	{
-		size_t left_height = binary_tree_height(tree->left);
-		size_t  right_height = binary_tree_height(tree->right);
-		if (left_height >= right_height)
-			return (left_height + 1);
-		else
-			return (right_height + 1);
-	}
-	return 0;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height >= right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
